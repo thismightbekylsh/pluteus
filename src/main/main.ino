e6f838a3dd08ab2fc2a0e63f98d1364b90cbdca9
Kaylan Barbosa Sampaio <thismightbekylsh@gmail.com>
2026-07-31 15:35:05 -0300

    feat: Corriginfo seguir a linha

--- src/main/main.ino
+++ src/main/main.ino
@@ -58,6 +58,8 @@ void setup() {
   Serial.begin(9600);
 }
 
+int Tspeed = 1;
+
 void loop() {
 
   SensorsCheck();
@@ -68,9 +70,9 @@ void loop() {
   int SeIR3 = analogRead(SIR3);
   int SeIR4 = analogRead(SIR4);
 
-  const int THRESHOLD = 500;
+  const int THRESHOLD = 450;
 
-  if(SeIR1 >= THRESHOLD && SeIR2 >= THRESHOLD)
+  if(SeIR1 >= THRESHOLD && SeIR2 >= THRESHOLD && SeIR3 != THRESHOLD && SeIR4 != THRESHOLD)
   {
     Stop();
     delay(500);
@@ -79,7 +81,7 @@ void loop() {
     Go(50,50,50,50);
     delay(600);
   }
-  else if(SeIR4 >= THRESHOLD && SeIR3 >= THRESHOLD)
+  else if(SeIR4 >= THRESHOLD && SeIR3 >= THRESHOLD && SeIR1 != THRESHOLD && SeIR2 != THRESHOLD)
   {
     Stop();
     delay(500);
@@ -88,23 +90,33 @@ void loop() {
     Go(50,50,50,50);
     delay(600);
   }
+  else if (Tspeed % 10 == 0)
+  {
+    Stop();
+    Tspeed = 1;
+  }
   else if(SeIR2 >= THRESHOLD)
   {
-    Left(100,60,100,60);
+    Left(110,70,110,70);
+    Tspeed++;
   }
   else if (SeIR3 >= THRESHOLD)
   {
-    Right(60,100,60,100);
+    Right(70,110,70,110);
+    Tspeed++;
   }
   else if (SeIR4 >= THRESHOLD)
   {
-    Right(60,100,60,100);
+    Right(70,110,70,110);
+    Tspeed++;
   }
   else if (SeIR1 >= THRESHOLD)
   {
-    Left(100,60,100,60);
+    Left(120,70,120,70);
+    Tspeed++;
   }
   else {
-    Go(62,62,62,62); //talvez aumentar a velocidade para que ele não fique parando
+    Go(70,70,70,70); //talvez aumentar a velocidade para que ele não fique parando
+    Tspeed++;
   }
 }
